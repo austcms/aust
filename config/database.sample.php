bbb813c32b0cba634fcb1af5bfcd19ac418d92b3
Alexandre de Oliveira <chavedomundo@gmail.com>
2010-04-13 15:02:10 -0300

    Arquivo padrão database.sample.php criado.

---
+++ config/database.sample.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Configurações de acesso ao Banco de Dados
+ *
+ */
+/**
+ * Conexão local de acesso ao DB com conteúdos
+ */
+
+class DATABASE_CONFIG
+{
+    static $dbConn = array(
+        'server' => 'localhost',
+        'port' => '8888',
+        'database' => 'aust',
+        'username' => 'root',
+        'password' => 'root',
+        'encoding' => 'utf8',
+    );
+}
+
+?>
