50cbe1324c9978f890e8dc2d06cf870a9de7b876
Alexandre de Oliveira <chavedomundo@gmail.com>
2011-06-11 05:38:19 -0300

    Observations on module Orders.

---
+++ modulos/orders/core/migrations/Migration_20110609021000_AddObservationFields.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * MOD MIGRATION
+ * 
+ * Migration de um módulos
+ * 
+ */
+class Migration_20110609021000_AddObservationFields extends Migrations
+{
+    function up(){
+
+        $schema = array(
+            'table' => "st_orders",
+            'field' => 'observation',
+            'type' => 'text',
+            'position' => 'AFTER sent'
+        );
+    		$this->addField($schema);
+
+        $schema = array(
+            'table' => "st_order_items",
+            'field' => 'observation',
+            'type' => 'text',
+            'position' => 'AFTER quantity_unit'
+        );
+    		$this->addField($schema);
+
+        return true;
+    }
+
+    function down(){
+        $this->dropField('st_orders', 'observation');
+        $this->dropField('st_order_items', 'observation');
+        return true;
+    }
+}
+?>
